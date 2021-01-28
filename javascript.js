e736917770092a4f66a95ea7159a13e1b9a5ef68
chiranda <78127136+chiranda@users.noreply.github.com>
2021-01-28 09:15:27 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+	function hello(){
+		alert("Please refresh this page");
+	}
+	function myFunction(){
+		location.reload();
+	}
+	function getData(var1, var2){
+	       	full = var1.value + var2.value; 
+	        alert("Your Name is " + full);
+	}
+	
